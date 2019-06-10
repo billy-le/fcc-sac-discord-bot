b21bb1e05b2bf02ea67f662ab265bf173b9d0fb7
Billy Le <lebilly87@gmail.com>
2019-06-10 02:04:13 -0700

    login: removed login routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,11 @@ import express from 'express';
 import homeRoute from './home';
 import webhookRoutes from './webhook';
 import oauthRoutes from './oauth';
-import loginRoutes from './login';
 
 const router = express.Router();
 
 router.use('/', homeRoute);
 router.use('/', webhookRoutes);
 router.use('/', oauthRoutes);
-router.use('/', loginRoutes);
 
 export default router;
